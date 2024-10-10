06c89f5899d8b1db34e9e3ee70ca606860182da1
MYH20009 <uakp.eng09@gmail.com>
2024-10-10 20:58:05 +0630

    Update hammer.py
    I changed damage

--- hammer.py
+++ hammer.py
@@ -79,8 +79,8 @@ def usage():
 	usage : python3 hammer.py [-s] [-p] [-t]
 	-h : help
 	-s : server ip
-	-p : port default 80
-	-t : turbo default 135 \033[0m''')
+	-p : port default 8080
+	-t : turbo default 185 \033[0m''')
 	sys.exit()
 
 
@@ -92,8 +92,8 @@ def get_parameters():
 	optp = OptionParser(add_help_option=False,epilog="Hammers")
 	optp.add_option("-q","--quiet", help="set logging to ERROR",action="store_const", dest="loglevel",const=logging.ERROR, default=logging.INFO)
 	optp.add_option("-s","--server", dest="host",help="attack to server ip -s ip")
-	optp.add_option("-p","--port",type="int",dest="port",help="-p 80 default 80")
-	optp.add_option("-t","--turbo",type="int",dest="turbo",help="default 135 -t 135")
+	optp.add_option("-p","--port",type="int",dest="port",help="-p 8080 default 8080")
+	optp.add_option("-t","--turbo",type="int",dest="turbo",help="default 185 -t 186")
 	optp.add_option("-h","--help",dest="help",action='store_true',help="help you")
 	opts, args = optp.parse_args()
 	logging.basicConfig(level=opts.loglevel,format='%(levelname)-8s %(message)s')
@@ -104,11 +104,11 @@ def get_parameters():
 	else:
 		usage()
 	if opts.port is None:
-		port = 80
+		port = 8080
 	else:
 		port = opts.port
 	if opts.turbo is None:
-		thr = 135
+		thr = 185
 	else:
 		thr = opts.turbo
 
